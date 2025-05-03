02ceedb15ccf0bf27b45c75d850f657a382f2478
shubh <shubhsharma23nov@gmail.com>
2025-05-03 12:04:18 +0530

    Updated day-2

--- src/main/java/com/example/demo/controller/RequestControllerApi.java
+++ src/main/java/com/example/demo/controller/RequestControllerApi.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +23,30 @@ public class RequestControllerApi{
 		}
 		return "";	
 	}
-	
+	@RequestMapping("/PathVar-test-1")
+	public String test1() {
+		List<String> li=new ArrayList<>();
+		li.add("Emp-1");
+		li.add("Emp-2");
+		li.add("Emp-3");
+		li.add("Emp-4");
+		li.add("Emp-5");
+		return li.toString();
+	}
+	@RequestMapping("/RequestParam-test")
+	public String test(@RequestParam(required=false) String id) {
+		List<String> li=new ArrayList<>();
+		li.add("Emp-1");
+		li.add("Emp-2");
+		li.add("Emp-3");
+		li.add("Emp-4");
+		li.add("Emp-5");
+		if(id!=null) {
+			for(String s:li) {
+				if(s.equals(id)) return s;
+			}
+			return "";
+		}
+		return li.toString();
+	}
 }
